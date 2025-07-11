01586729281bb7fd96220bf566597ef924d7e85b
nicolas acard <nicolasacard@Ordinateur-portable-de-nicolas.local>
2025-07-11 15:23:48 +0200

    initial release

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import { useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  useEffect(() => {
+    const getUsers = async() => {
+      const users = await axios.get('https://tangible-frog-production.up.railway.app/users')
+      console.log(users)
+    }
+    getUsers()
+  }, [])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>App deployed on production !</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      
+    </div>
+  );
+};
 
-export default App
+export default App;
